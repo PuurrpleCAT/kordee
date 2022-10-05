8dc8a406a2d8b087ddf574793a1c12d04b1e91f9
Andy Du <andy.du1000@outlook.com>
2022-10-06 01:48:20 +1100

    did multithreading, turns out to be slower than single threading

--- src/data.rs
+++ src/data.rs
@@ -8,5 +8,10 @@ const DURATION: u8 = 2;
 const AMPLITUDE: f64 = 0.5;
 
 pub fn write(wav: &mut File, sample_rate: u32, bits_per_sample: u16) -> Result<u64> {
-    Chord::play("F4 A4 C5", wav, bits_per_sample, sample_rate, AMPLITUDE, DURATION)
+    Chord::play("F4 A4 C5", wav, bits_per_sample, sample_rate, AMPLITUDE, DURATION).unwrap();
+    Chord::play("F4 A4 D5", wav, bits_per_sample, sample_rate, AMPLITUDE, DURATION).unwrap();
+    Chord::play("D4 F4 Bb4", wav, bits_per_sample, sample_rate, AMPLITUDE, DURATION).unwrap();
+    Chord::play("E4 G4 C5", wav, bits_per_sample, sample_rate, AMPLITUDE, DURATION)
 }
+// makes this sort of BUP sound with every chord change because the next chord starts at 0 pitch, and that can be heard
+
--- src/notes.rs
+++ src/notes.rs
@@ -1,8 +1,332 @@
 pub trait Note {fn f(&self) -> f32;}
-pub struct A4;
-pub struct F4;
-pub struct C5;
-impl Note for F4 {fn f(&self) -> f32{349.2282}}
-impl Note for A4 {fn f(&self) -> f32{440.0000}}
-impl Note for C5 {fn f(&self) -> f32{523.2511}}
+pub struct A0 ;
+pub struct As0;
+pub struct Bb0;
+pub struct B0 ;
+pub struct Bs1;
+pub struct Cb0;
 
+pub struct C1 ;
+pub struct Cs1;
+pub struct Db1;
+pub struct D1 ;
+pub struct Ds1;
+pub struct Eb1;
+pub struct E1 ;
+pub struct Es1;
+pub struct Fb1;
+pub struct F1 ;
+pub struct Fs1;
+pub struct Gb1;
+pub struct G1 ;
+pub struct Gs1;
+pub struct Ab1;
+pub struct A1 ;
+pub struct As1;
+pub struct Bb1;
+pub struct B1 ;
+pub struct Bs2;
+pub struct Cb1;
+
+pub struct C2 ;
+pub struct Cs2;
+pub struct Db2;
+pub struct D2 ;
+pub struct Ds2;
+pub struct Eb2;
+pub struct E2 ;
+pub struct Es2;
+pub struct Fb2;
+pub struct F2 ;
+pub struct Fs2;
+pub struct Gb2;
+pub struct G2 ;
+pub struct Gs2;
+pub struct Ab2;
+pub struct A2 ;
+pub struct As2;
+pub struct Bb2;
+pub struct B2 ;
+pub struct Bs3;
+pub struct Cb2;
+
+
+pub struct C3 ;
+pub struct Cs3;
+pub struct Db3;
+pub struct D3 ;
+pub struct Ds3;
+pub struct Eb3;
+pub struct E3 ;
+pub struct Es3;
+pub struct Fb3;
+pub struct F3 ;
+pub struct Fs3;
+pub struct Gb3;
+pub struct G3 ;
+pub struct Gs3;
+pub struct Ab3;
+pub struct A3 ;
+pub struct As3;
+pub struct Bb3;
+pub struct B3 ;
+pub struct Bs4;
+pub struct Cb3;
+
+
+pub struct C4 ;
+pub struct Cs4;
+pub struct Db4;
+pub struct D4 ;
+pub struct Ds4;
+pub struct Eb4;
+pub struct E4 ;
+pub struct Es4;
+pub struct Fb4;
+pub struct F4 ;
+pub struct Fs4;
+pub struct Gb4;
+pub struct G4 ;
+pub struct Gs4;
+pub struct Ab4;
+pub struct A4 ;
+pub struct As4;
+pub struct Bb4;
+pub struct B4 ;
+pub struct Bs5;
+pub struct Cb4;
+
+
+pub struct C5 ;
+pub struct Cs5;
+pub struct Db5;
+pub struct D5 ;
+pub struct Ds5;
+pub struct Eb5;
+pub struct E5 ;
+pub struct Es5;
+pub struct Fb5;
+pub struct F5 ;
+pub struct Fs5;
+pub struct Gb5;
+pub struct G5 ;
+pub struct Gs5;
+pub struct Ab5;
+pub struct A5 ;
+pub struct As5;
+pub struct Bb5;
+pub struct B5 ;
+pub struct Bs6;
+pub struct Cb5;
+
+
+pub struct C6 ;
+pub struct Cs6;
+pub struct Db6;
+pub struct D6 ;
+pub struct Ds6;
+pub struct Eb6;
+pub struct E6 ;
+pub struct Es6;
+pub struct Fb6;
+pub struct F6 ;
+pub struct Fs6;
+pub struct Gb6;
+pub struct G6 ;
+pub struct Gs6;
+pub struct Ab6;
+pub struct A6 ;
+pub struct As6;
+pub struct Bb6;
+pub struct B6 ;
+pub struct Bs7;
+pub struct Cb6;
+
+
+pub struct C7 ;
+pub struct Cs7;
+pub struct Db7;
+pub struct D7 ;
+pub struct Ds7;
+pub struct Eb7;
+pub struct E7 ;
+pub struct Es7;
+pub struct Fb7;
+pub struct F7 ;
+pub struct Fs7;
+pub struct Gb7;
+pub struct G7 ;
+pub struct Gs7;
+pub struct Ab7;
+pub struct A7 ;
+pub struct As7;
+pub struct Bb7;
+pub struct B7 ;
+pub struct Bs8;
+pub struct Cb7;
+
+
+pub struct C8 ;
+
+impl Note for A0 {fn f(&self) -> f32 {27.50000}}
+impl Note for As0 {fn f(&self) -> f32 {29.13524}}
+impl Note for Bb0 {fn f(&self) -> f32 {29.13524}}
+impl Note for B0 {fn f(&self) -> f32 {30.86771}}
+impl Note for Bs1 {fn f(&self) -> f32 {32.70320}}
+impl Note for Cb0 {fn f(&self) -> f32 {30.86771}}
+
+impl Note for C1 {fn f(&self) -> f32 {32.70320}}
+impl Note for Cs1 {fn f(&self) -> f32 {34.64783}}
+impl Note for Db1 {fn f(&self) -> f32 {34.64783}}
+impl Note for D1 {fn f(&self) -> f32 {36.70810}}
+impl Note for Ds1 {fn f(&self) -> f32 {38.89087}}
+impl Note for Eb1 {fn f(&self) -> f32 {38.89087}}
+impl Note for E1 {fn f(&self) -> f32 {41.20344}}
+impl Note for Es1 {fn f(&self) -> f32 {43.65353}}
+impl Note for Fb1 {fn f(&self) -> f32 {41.20344}}
+impl Note for F1 {fn f(&self) -> f32 {43.65353}}
+impl Note for Fs1 {fn f(&self) -> f32 {46.24930}}
+impl Note for Gb1 {fn f(&self) -> f32 {46.24930}}
+impl Note for G1 {fn f(&self) -> f32 {48.99943}}
+impl Note for Gs1 {fn f(&self) -> f32 {51.91309}}
+impl Note for Ab1 {fn f(&self) -> f32 {51.91309}}
+impl Note for A1 {fn f(&self) -> f32 {55.00000}}
+impl Note for As1 {fn f(&self) -> f32 {58.27047}}
+impl Note for Bb1 {fn f(&self) -> f32 {58.27047}}
+impl Note for B1 {fn f(&self) -> f32 {61.73541}}
+impl Note for Bs2 {fn f(&self) -> f32 {65.40639}}
+impl Note for Cb1 {fn f(&self) -> f32 {61.73541}}
+
+impl Note for C2 {fn f(&self) -> f32 {65.40639}}
+impl Note for Cs2 {fn f(&self) -> f32 {69.29566}}
+impl Note for Db2 {fn f(&self) -> f32 {69.29566}}
+impl Note for D2 {fn f(&self) -> f32 {73.41619}}
+impl Note for Ds2 {fn f(&self) -> f32 {77.78175}}
+impl Note for Eb2 {fn f(&self) -> f32 {77.78175}}
+impl Note for E2 {fn f(&self) -> f32 {82.40689}}
+impl Note for Es2 {fn f(&self) -> f32 {87.30706}}
+impl Note for Fb2 {fn f(&self) -> f32 {82.40689}}
+impl Note for F2 {fn f(&self) -> f32 {87.30706}}
+impl Note for Fs2 {fn f(&self) -> f32 {92.49861}}
+impl Note for Gb2 {fn f(&self) -> f32 {92.49861}}
+impl Note for G2 {fn f(&self) -> f32 {97.99886}}
+impl Note for Gs2 {fn f(&self) -> f32 {103.8262}}
+impl Note for Ab2 {fn f(&self) -> f32 {103.8262}}
+impl Note for A2 {fn f(&self) -> f32 {110.0000}}
+impl Note for As2 {fn f(&self) -> f32 {116.5409}}
+impl Note for Bb2 {fn f(&self) -> f32 {116.5409}}
+impl Note for B2 {fn f(&self) -> f32 {123.4708}}
+impl Note for Bs3 {fn f(&self) -> f32 {130.8128}}
+impl Note for Cb2 {fn f(&self) -> f32 {123.4708}}
+
+impl Note for C3 {fn f(&self) -> f32 {130.8128}}
+impl Note for Cs3 {fn f(&self) -> f32 {138.5913}}
+impl Note for Db3 {fn f(&self) -> f32 {138.5913}}
+impl Note for D3 {fn f(&self) -> f32 {146.8324}}
+impl Note for Ds3 {fn f(&self) -> f32 {155.5635}}
+impl Note for Eb3 {fn f(&self) -> f32 {155.5635}}
+impl Note for E3 {fn f(&self) -> f32 {164.8138}}
+impl Note for Es3 {fn f(&self) -> f32 {174.6141}}
+impl Note for Fb3 {fn f(&self) -> f32 {164.8138}}
+impl Note for F3 {fn f(&self) -> f32 {174.6141}}
+impl Note for Fs3 {fn f(&self) -> f32 {184.9972}}
+impl Note for Gb3 {fn f(&self) -> f32 {184.9972}}
+impl Note for G3 {fn f(&self) -> f32 {195.9977}}
+impl Note for Gs3 {fn f(&self) -> f32 {207.6523}}
+impl Note for Ab3 {fn f(&self) -> f32 {207.6523}}
+impl Note for A3 {fn f(&self) -> f32 {220.0000}}
+impl Note for As3 {fn f(&self) -> f32 {233.0819}}
+impl Note for Bb3 {fn f(&self) -> f32 {233.0819}}
+impl Note for B3 {fn f(&self) -> f32 {246.9417}}
+impl Note for Bs4 {fn f(&self) -> f32 {261.6256}}
+impl Note for Cb3 {fn f(&self) -> f32 {246.9417}}
+
+impl Note for C4 {fn f(&self) -> f32 {261.6256}}
+impl Note for Cs4 {fn f(&self) -> f32 {277.1826}}
+impl Note for Db4 {fn f(&self) -> f32 {277.1826}}
+impl Note for D4 {fn f(&self) -> f32 {293.6648}}
+impl Note for Ds4 {fn f(&self) -> f32 {311.1270}}
+impl Note for Eb4 {fn f(&self) -> f32 {311.1270}}
+impl Note for E4 {fn f(&self) -> f32 {329.6276}}
+impl Note for Es4 {fn f(&self) -> f32 {349.2282}}
+impl Note for Fb4 {fn f(&self) -> f32 {329.6276}}
+impl Note for F4 {fn f(&self) -> f32 {349.2282}}
+impl Note for Fs4 {fn f(&self) -> f32 {369.9944}}
+impl Note for Gb4 {fn f(&self) -> f32 {369.9944}}
+impl Note for G4 {fn f(&self) -> f32 {391.9954}}
+impl Note for Gs4 {fn f(&self) -> f32 {415.3047}}
+impl Note for Ab4 {fn f(&self) -> f32 {415.3047}}
+impl Note for A4 {fn f(&self) -> f32 {440.0000}}
+impl Note for As4 {fn f(&self) -> f32 {466.1638}}
+impl Note for Bb4 {fn f(&self) -> f32 {466.1638}}
+impl Note for B4 {fn f(&self) -> f32 {493.8833}}
+impl Note for Bs5 {fn f(&self) -> f32 {523.2511}}
+impl Note for Cb4 {fn f(&self) -> f32 {493.8833}}
+
+impl Note for C5 {fn f(&self) -> f32 {523.2511}}
+impl Note for Cs5 {fn f(&self) -> f32 {554.3653}}
+impl Note for Db5 {fn f(&self) -> f32 {554.3653}}
+impl Note for D5 {fn f(&self) -> f32 {587.3295}}
+impl Note for Ds5 {fn f(&self) -> f32 {622.2540}}
+impl Note for Eb5 {fn f(&self) -> f32 {622.2540}}
+impl Note for E5 {fn f(&self) -> f32 {659.2551}}
+impl Note for Es5 {fn f(&self) -> f32 {698.4565}}
+impl Note for Fb5 {fn f(&self) -> f32 {659.2551}}
+impl Note for F5 {fn f(&self) -> f32 {698.4565}}
+impl Note for Fs5 {fn f(&self) -> f32 {739.9888}}
+impl Note for Gb5 {fn f(&self) -> f32 {739.9888}}
+impl Note for G5 {fn f(&self) -> f32 {783.9909}}
+impl Note for Gs5 {fn f(&self) -> f32 {830.6094}}
+impl Note for Ab5 {fn f(&self) -> f32 {830.6094}}
+impl Note for A5 {fn f(&self) -> f32 {880.0000}}
+impl Note for As5 {fn f(&self) -> f32 {932.3275}}
+impl Note for Bb5 {fn f(&self) -> f32 {932.3275}}
+impl Note for B5 {fn f(&self) -> f32 {987.7666}}
+impl Note for Bs6 {fn f(&self) -> f32 {1046.502}}
+impl Note for Cb5 {fn f(&self) -> f32 {987.7666}}
+
+impl Note for C6 {fn f(&self) -> f32 {1046.502}}
+impl Note for Cs6 {fn f(&self) -> f32 {1108.731}}
+impl Note for Db6 {fn f(&self) -> f32 {1108.731}}
+impl Note for D6 {fn f(&self) -> f32 {1174.659}}
+impl Note for Ds6 {fn f(&self) -> f32 {1244.508}}
+impl Note for Eb6 {fn f(&self) -> f32 {1244.508}}
+impl Note for E6 {fn f(&self) -> f32 {1318.510}}
+impl Note for Es6 {fn f(&self) -> f32 {1396.913}}
+impl Note for Fb6 {fn f(&self) -> f32 {1318.510}}
+impl Note for F6 {fn f(&self) -> f32 {1396.913}}
+impl Note for Fs6 {fn f(&self) -> f32 {1479.978}}
+impl Note for Gb6 {fn f(&self) -> f32 {1479.978}}
+impl Note for G6 {fn f(&self) -> f32 {1567.982}}
+impl Note for Gs6 {fn f(&self) -> f32 {1661.219}}
+impl Note for Ab6 {fn f(&self) -> f32 {1661.219}}
+impl Note for A6 {fn f(&self) -> f32 {1760.000}}
+impl Note for As6 {fn f(&self) -> f32 {1864.655}}
+impl Note for Bb6 {fn f(&self) -> f32 {1864.655}}
+impl Note for B6 {fn f(&self) -> f32 {1975.533}}
+impl Note for Bs7 {fn f(&self) -> f32 {2093.005}}
+impl Note for Cb6 {fn f(&self) -> f32 {1975.533}}
+
+impl Note for C7 {fn f(&self) -> f32 {2093.005}}
+impl Note for Cs7 {fn f(&self) -> f32 {2217.461}}
+impl Note for Db7 {fn f(&self) -> f32 {2217.461}}
+impl Note for D7 {fn f(&self) -> f32 {2349.318}}
+impl Note for Ds7 {fn f(&self) -> f32 {2489.016}}
+impl Note for Eb7 {fn f(&self) -> f32 {2489.016}}
+impl Note for E7 {fn f(&self) -> f32 {2637.020}}
+impl Note for Es7 {fn f(&self) -> f32 {2793.826}}
+impl Note for Fb7 {fn f(&self) -> f32 {2637.020}}
+impl Note for F7 {fn f(&self) -> f32 {2793.826}}
+impl Note for Fs7 {fn f(&self) -> f32 {2959.955}}
+impl Note for Gb7 {fn f(&self) -> f32 {2959.955}}
+impl Note for G7 {fn f(&self) -> f32 {3135.963}}
+impl Note for Gs7 {fn f(&self) -> f32 {3322.438}}
+impl Note for Ab7 {fn f(&self) -> f32 {3322.438}}
+impl Note for A7 {fn f(&self) -> f32 {3520.000}}
+impl Note for As7 {fn f(&self) -> f32 {3729.310}}
+impl Note for Bb7 {fn f(&self) -> f32 {3729.310}}
+impl Note for B7 {fn f(&self) -> f32 {3951.066}}
+impl Note for Bs8 {fn f(&self) -> f32 {4186.009}}
+impl Note for Cb7 {fn f(&self) -> f32 {3951.066}}
+
+impl Note for C8 {fn f(&self) -> f32 {4186.009}}
